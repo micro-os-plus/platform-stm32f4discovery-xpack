e8baeb5b3564ee262542786e083e80db27a0ea61
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:46:59 +0200

    device.h: silence warnings

--- device/include/micro-os-plus/device.h
+++ device/include/micro-os-plus/device.h
@@ -31,7 +31,19 @@
 // ----------------------------------------------------------------------------
 
 #include <micro-os-plus/architecture.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpadded"
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#if defined(__cplusplus)
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wuseless-cast"
+#pragma GCC diagnostic ignored "-Wvolatile"
+#endif
+
 #include "stm32f4xx.h"
+
+#pragma GCC diagnostic pop
 
 // ----------------------------------------------------------------------------
 
